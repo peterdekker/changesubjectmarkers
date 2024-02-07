5fa45a55171e7122436e00f00bf71dce76a35b36
Peter Dekker <pdekker@ai.vub.ac.be>
2024-02-07 13:35:09 +0100

    MIT license

--- analyse_data.py
+++ analyse_data.py
@@ -1,23 +1,3 @@
-
-# changesubjectmarkers: Perform statistical analysis of change between proto and modern forms of person markers.
-# Copyright (C) 2024  Peter Dekker, Sonja Gipper, Bart de Boer
-
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-# Input data expected to be in: data/verbal_person-number_indexes_merged.csv
-# See README for more information on installation.
-
 import pandas as pd
 import editdistance
 import seaborn as sns
